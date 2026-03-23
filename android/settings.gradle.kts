a43f80876b943d9db935794053130153dcf01174
ImBajrangi <sharmagharsh1@gmail.com>
2026-03-23 19:03:41 +0530

    Downgrade flutter_local_notifications to version 17.1.0 and remove its Windows plugin entry.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+rootProject.name = "foody_vrinda_v3"
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -20,7 +22,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
